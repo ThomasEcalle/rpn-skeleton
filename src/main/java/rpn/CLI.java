4db3cb0a5163d2ce1bb99955bb7b81de92187264
ThomasEcalle <thomasecalle@hotmail.fr>
2018-03-16 11:56:00 +0100

    aded complex implementation

--- src/main/java/rpn/CLI.java
+++ src/main/java/rpn/CLI.java
@@ -1,13 +1,153 @@
 package rpn;
 
 
-import java.util.Stack;
+import java.util.*;
+import java.util.function.DoubleBinaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class CLI {
+public class CLI
+{
+    public static class ArgumentsFormatter
+    {
+        private String arguments;
 
-    public static final void main(String[] args) {
+        public ArgumentsFormatter(String arguments)
+        {
+
+            this.arguments = arguments;
+        }
+
+        public Stack<String> format()
+        {
+            //TODO: Check arguments
+            //TODO: Stream Java8
+
+            final Stack<String> stack = new Stack<>();
+
+            final List<String> list = Arrays.asList(arguments.split(" "));
+            Collections.reverse(list);
+            stack.addAll(list);
+
+            return stack;
+        }
+    }
+
+    public static class Calculator
+    {
+
+        private Stack<String> operables;
+
+        public Calculator(Stack<String> operables)
+        {
+            this.operables = operables;
+        }
+
+        public double evaluate()
+        {
+            final Stack<Double> stack = new Stack<>();
+
+            while (!operables.isEmpty())
+            {
+                final String operable = operables.pop();
+                if (Operator.isOperator(operable))
+                {
+                    final Operator operator = Operator.getFromString(operable);
+
+                    final Double second = stack.pop();
+                    final Double first = stack.pop();
+
+                    if (second == null || first == null)
+                    {
+                        throw new IllegalArgumentException("Il manque au moins une opérande pour que l'opération soit correcte");
+                    }
+
+                    final Double result = operator.operate(first, second);
+                    stack.push(result);
+                } else
+                {
+                    stack.push(Double.valueOf(operable));
+                }
+
+            }
+
+            if (stack.size() == 1)
+            {
+                return stack.pop();
+            } else
+            { //TODO print error
+                throw new IllegalStateException("" + Stream.of(stack.toArray()).map(Object::toString).collect(Collectors.joining(" ")));
+            }
+
+        }
+    }
+
+    public enum Operator
+    {
+
+        PLUS("+", Double::sum),
+        MINUS("-", (left, right) -> left - right),
+        TIMES("*", (left, right) -> left * right),
+        DIVIDE("/", (left, right) -> right != 0., (left, right) -> left / right),
+        POWER("**", (left, right) -> Math.pow(left, right));
+
+        private interface OperandChecker
+        {
+            boolean isValid(Double left, Double right);
+        }
+
+        private static Map<String, Operator> lookup = new HashMap<>();
+
+        static
+        {
+            for (Operator operator : Operator.values())
+            {
+                lookup.put(operator.operator, operator);
+            }
+        }
+
+
+        private final String operator;
+        private final OperandChecker operandChecker;
+        private final DoubleBinaryOperator operation;
+
+        Operator(String operator, OperandChecker operandChecker, DoubleBinaryOperator operation)
+        {
+            this.operator = operator;
+            this.operandChecker = operandChecker;
+            this.operation = operation;
+        }
+
+        Operator(String operator, DoubleBinaryOperator operation)
+        {
+            this.operator = operator;
+            this.operation = operation;
+            this.operandChecker = null;
+        }
+
+        public double operate(Double left, Double right)
+        {
+            if (left != null && right != null && (operandChecker == null || operandChecker.isValid(left, right)))
+            {
+                return this.operation.applyAsDouble(left, right);
+            }
+
+            throw new IllegalArgumentException("");
+        }
+
+        public static Operator getFromString(String operator)
+        {
+            return lookup.get(operator);
+        }
+
+        public static boolean isOperator(String operator)
+        {
+            return getFromString(operator) != null;
+        }
+    }
+
+    public static final void main(String[] args)
+    {
         String expression = Stream.of(args).collect(Collectors.joining(" "));
         System.out.println("About to evaluate '" + expression + "'");
 
@@ -17,54 +157,29 @@ public class CLI {
             return;
         }
 
-        double result;
-        try {
-            result = evaluate(expression);
-            System.out.println("> " + result);
-        } catch (ArithmeticException exception) {
+        try
+        {
+            System.out.println("> " + evaluate(expression));
+        }
+        catch (ArithmeticException exception)
+        {
+            System.out.printf("error while calculating : " + exception.getMessage());
+            exception.printStackTrace();
+        }
+        catch (IllegalArgumentException exception)
+        {
             System.out.printf("error while calculating : " + exception.getMessage());
             exception.printStackTrace();
         }
     }
 
-    static double evaluate(String expression) throws ArithmeticException {
-        final Stack<Double> stack = new Stack<>();
-        final String[] calculatedValues = expression.split(" ");
+    static double evaluate(String expression) throws ArithmeticException
+    {
+        final ArgumentsFormatter argumentsFormatter = new ArgumentsFormatter(expression);
 
-        for (String operation : calculatedValues) {
-            if (isOperator(operation)) {
-                final Double second = stack.pop();
-                final Double first = stack.pop();
+        final Calculator calculator = new Calculator(argumentsFormatter.format());
 
-                final Double result = operate(first, second, operation);
-                stack.push(result);
-            } else {
-                stack.push(Double.valueOf(operation));
-            }
-        }
+        return calculator.evaluate();
 
-        return stack.pop();
-    }
-
-    private static Double operate(Double value1, Double value2, String operator) throws ArithmeticException {
-        switch (operator) {
-            case "*":
-                return value1 * value2;
-            case "+":
-                return value1 + value2;
-
-            case "-":
-                return value1 - value2;
-
-            default:
-                if (value2 == 0) {
-                    throw new ArithmeticException("Impossible de diviser par 0");
-                }
-                return value1 / value2;
-        }
-    }
-
-    private static boolean isOperator(String operation) {
-        return operation.equals("*") || operation.equals("+") || operation.equals("-") || operation.equals("/");
     }
 }
